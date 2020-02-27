b21ed00525af7dce95ba01474a6abdae9a332c16
luccastraumer <cc19372@g.unicamp.br>
2020-02-27 17:33:50 -0300

    more ex done

--- clock/src/main/java/Clock.java
+++ clock/src/main/java/Clock.java
@@ -3,20 +3,37 @@ public class Clock{
     private int hours;
     private int minutes;
     public Clock(int hours,int minutes){
-
+        this.hours = hours;
+        this.minutes = minutes;
     }
     public void add(int value){
+        
 
+    }
+    public boolean equals(Object obj){
+        if(obj == null)
+            return false;
+        if(obj == this)
+            return true;
+        if(obj.getClass() != this.getClass())
+            return false;
+        Clock aux = (Clock)obj;
+        if(aux.minutes != this.minutes)
+            return false;
+        if(aux.hours != this.hours)
+            return false;
+
+        return true;
     }
     public String toString(){
         if(hours >= 10){
             if(minutes >= 10)
                 return this.hours +":"+ this.minutes;
-            return this.hours +": 0"+this.minutes;
+            return this.hours +":0"+this.minutes;
         }else {
             if(minutes >= 10)
                 return "0"+this.hours + ":"+this.minutes;
-            return "0"+this.hours+": 0"+this.minutes;
+            return "0"+this.hours+":0"+this.minutes;
         }
     }
 }
