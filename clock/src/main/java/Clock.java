ff482375a65ec1b37ff195056b760f219cfc1b5f
luccastraumer <cc19372@g.unicamp.br>
2020-03-02 11:43:51 -0300

    finaly clock is done

--- clock/src/main/java/Clock.java
+++ clock/src/main/java/Clock.java
@@ -2,15 +2,79 @@ public class Clock{
 
     private int hours;
     private int minutes;
-    public Clock(int hours,int minutes){
-        this.hours = hours;
-        this.minutes = minutes;
-    }
-    public void add(int value){
-        
 
+
+
+    public Clock(int hours, int minutes){
+        this.minutes = manyMin(minutes);
+        this.hours = manyHours(hours);
     }
-    public boolean equals(Object obj){
+
+    private int manyMin(int min){
+        int hours = 0;
+        int minute = this.minutes;
+        if(min > 0) {
+            while (min != 0) {
+                minute++;
+                min--;
+                if(minute >= 60){
+                    hours++;
+                    minute=0;
+                }
+            }
+            this.hours = manyHours(hours);
+        }else{
+            while(min != 0){
+                minute--;
+                min++;
+                if(minute < 0){
+                    hours--;
+                    minute=59;
+                }
+            }
+            this.hours = manyHours(hours);
+        }
+
+        return minute;
+    }
+    private int manyHours(int hours){
+        int hour = this.hours;
+        if(hours > 0){
+            while(hours != 0) {
+                hour++;
+                hours--;
+                if (hour > 23) {
+                    hour = 0;
+                }
+            }
+        }else{
+            while(hours != 0) {
+                hour--;
+                hours++;
+                if (hour < 0) {
+                    hour = 23;
+                }
+            }
+        }
+        return hour;
+    }
+
+    public void add(int minutes){
+        this.minutes = manyMin(minutes);
+        System.out.println();
+    }
+    public String toString(){
+        String horas = this.hours+"";
+        String minutes = this.minutes+"";
+
+        horas = horas.length() > 1 ? horas+":" : "0"+horas+":";
+        minutes =minutes.length() > 1 ? minutes : "0"+minutes;
+
+        return horas + minutes;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
         if(obj == null)
             return false;
         if(obj == this)
@@ -24,16 +88,5 @@ public class Clock{
             return false;
 
         return true;
-    }
-    public String toString(){
-        if(hours >= 10){
-            if(minutes >= 10)
-                return this.hours +":"+ this.minutes;
-            return this.hours +":0"+this.minutes;
-        }else {
-            if(minutes >= 10)
-                return "0"+this.hours + ":"+this.minutes;
-            return "0"+this.hours+":0"+this.minutes;
-        }
     }
 }
